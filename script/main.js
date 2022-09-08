a868e555d0e2e17f370145e24e52b3cda67a14ce
Jessarey <jessareyapuli@gmial.com>
2022-09-08 14:16:14 +0800

    JS

---
+++ script/main.js
@@ -0,0 +1,9 @@
+const boxes = document.querySelector('.box');
+const statusEl = document.querySelector('#status p');
+
+class Player {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+}
